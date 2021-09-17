7e16fee00fd79908a4857f91f3f59a141968beb2
armaanshaukat <armaanarain21@gmail.com>
2021-09-16 23:43:38 -0500

    Command Pattern for Printers

---
+++ src/Printer.java
@@ -0,0 +1,22 @@
+
+public class Printer {
+	
+	public Printer() {
+	}
+	
+	public void printOn() {
+		System.out.println("Printer is getting toner to temperature");
+	}
+	
+	public void printSleep() {
+		System.out.println("Printer is going to sleep mode");
+	}
+	
+	public void printLandscape() {
+		System.out.println("Printing in landscape orientation");
+	}
+	
+	public void printPotrait() {
+		System.out.println("Printing in portrait orientation");
+	}
+}
